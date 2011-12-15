ed38ed32960616317cf23cbffe156354a17e5319
Philip Abbet <philip.abbet@gmail.com>
2011-12-15 09:31:12 +0100

    [Unittests] Add some new JS test functions

--- unittests/js-test-runner/tests.js
+++ unittests/js-test-runner/tests.js
@@ -7,6 +7,9 @@ function CHECK(test, message)
 
 function CHECK_EQUAL(wanted, value)
 {
+    if (isNaN(value) && !isNaN(wanted))
+        throw "Expected " + wanted + ", got " + value;
+
     if (value != wanted)
         throw "Expected " + wanted + ", got " + value;
 }
@@ -16,7 +19,10 @@ function CHECK_CLOSE(wanted, value, epsilon)
 {
     if (epsilon == undefined)
         epsilon = 1e-6;
-    
+
+    if (isNaN(value) && !isNaN(wanted))
+        throw "Expected " + wanted + " +/- " + epsilon + ", got " + value;
+
     if ((value < wanted - epsilon) || (value > wanted + epsilon))
         throw "Expected " + wanted + " +/- " + epsilon + ", got " + value;
 }
